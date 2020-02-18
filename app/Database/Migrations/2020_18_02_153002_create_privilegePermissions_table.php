cd24059e041e6a7b3e74b1bcc76cb1de5f486c26
Brad Martin <bradmartin@brads-macbook-2.lan>
2020-02-18 16:48:36 +1100

    Fix

--- app/Database/Migrations/2020_18_02_153002_create_privilegePermissions_table.php
+++ app/Database/Migrations/2020_18_02_153002_create_privilegePermissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGroupedPermissionsTable extends Migration
+class CreatePrivilegePermissionsTable extends Migration
 {
     /**
      * Run the migrations.
